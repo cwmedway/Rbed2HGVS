8b6dec6180fe41138455ec2c7ef67de68e9d2302
cwmedway <cwm.bioinformatics@gmail.com>
2020-10-10 19:12:30 +0100

    refactored hgvs calculation

--- R/Rbed2HGVS.R
+++ R/Rbed2HGVS.R
@@ -1,11 +1,11 @@
 
-makeCdsDb <- function() {
-
-  ucsc_hg19_ncbiRefSeq <- GenomicFeatures::makeTxDbFromUCSC(genome = "hg19", tablename = "ncbiRefSeq") %>%
-    GenomeInfoDb::keepStandardChromosomes(.)
-  GenomeInfoDb::seqlevelsStyle(ucsc_hg19_ncbiRefSeq) <- "NCBI"
-  AnnotationDbi::saveDb(x = ucsc_hg19_ncbiRefSeq, file = './data/ucsc_hg19_ncbiRefSeq.sqlite')
-}
+# makeCdsDb <- function() {
+#
+#   ucsc_hg19_ncbiRefSeq <- GenomicFeatures::makeTxDbFromUCSC(genome = "hg19", tablename = "ncbiRefSeq") %>%
+#     GenomeInfoDb::keepStandardChromosomes(.)
+#   GenomeInfoDb::seqlevelsStyle(ucsc_hg19_ncbiRefSeq) <- "NCBI"
+#   AnnotationDbi::saveDb(x = ucsc_hg19_ncbiRefSeq, file = './data/ucsc_hg19_ncbiRefSeq.sqlite')
+# }
 
 #' appends an additional column of interval in HGVS nomenclature to a bedfile
 #'
@@ -29,153 +29,52 @@ Rbed2HGVS <- function(bedfile, db = './data/ucsc_hg19_ncbiRefSeq.sqlite') {
   # get cds indexed by refseq fron db
   cds_by_tx <- GenomicFeatures::cdsBy(x = ucsc_hg19_ncbiRefSeq, by = "tx", use.name = T)
 
-  # make separate ranges object for start and end coordinates
-  bedfile_start <- IRanges::narrow(x = bedfile, start = 1, width = 1)
-  bedfile_end   <- IRanges::narrow(x = bedfile, start = -1, width = 1)
-
   # get cds info for start and end of bedfile
-  hgvs_start <- getHgvs(bedfile = bedfile_start, cds = cds_by_tx)
-  hgvs_end   <- getHgvs(bedfile = bedfile_end, cds = cds_by_tx)
+  hgvs <- getHgvs(bedfile = bedfile, cds = cds_by_tx) %>% do.call(rbind, .)
 
-  # combine start and end for each bed entry
-  lapply(seq(bedfile), function(bed_i) {
+  hgvs$gene <- getSymbolRefseq(refSeqId = hgvs$tx)
 
-    # get bed line info
-    chr   <- GenomicRanges::seqnames(bedfile[bed_i]) %>% as.vector()
-    start <- GenomicRanges::start(bedfile[bed_i])
-    end   <- GenomicRanges::end(bedfile[bed_i])
-
-    # check if tx/start has a corresponding tx/end - ignore NA
-    s_tx <- hgvs_start[[bed_i]]$tx %>% as.vector() %>% .[!is.na(.)]
-    e_tx <- hgvs_end[[bed_i]]$tx %>% as.vector() %>% .[!is.na(.)]
-
-    # get tx missing cds for end
-    miss_e <- s_tx[!(s_tx %in% e_tx)]
-
-    # get tx missing cds for end
-    miss_s <-e_tx[!(e_tx %in% s_tx)]
-
-    if (!isEmpty(miss_s)) {
-      df_miss_s <- getUsDs(missing_tx = miss_s, bedfile = bedfile_start[bed_i], cds = cds_by_tx)
-      hgvs_start[[bed_i]] <- rbind(
-        hgvs_start[[bed_i]][stats::complete.cases(hgvs_start[[bed_i]]),],
-        df_miss_s
-        )
-    }
-
-    if (!isEmpty(miss_e)) {
-      df_miss_e <- getUsDs(missing_tx = miss_e, bedfile = bedfile_end[bed_i], cds = cds_by_tx)
-      hgvs_end[[bed_i]] <- rbind(
-        hgvs_end[[bed_i]][complete.cases(hgvs_end[[bed_i]]),],
-        df_miss_e
-        )
-    }
-
-    df <- merge.data.frame(
-      x = hgvs_start[[bed_i]],
-      y = hgvs_end[[bed_i]],
-      by = "tx",
-      all = TRUE, suffixes = c(".start",".end"))
-
-    gene <- getSymbolRefseq(refSeqId = df$tx)[,"SYMBOL"]
-
-    cbind(chr, start, end, gene, df) %>% return()
-    }) %>%
-    do.call(rbind, .)
+  return(hgvs)
 }
-
-
-#' getUsDs
-#'
-#' @param missing_tx character vector of transcript names
-#' @param bedfile GRanges object containing a single bedfile
-#' @param cds
-#'
-#' @return
-getUsDs <- function(missing_tx, bedfile, cds) {
-
-  out <- lapply(missing_tx, function(tx) {
-
-    out <- GenomicRanges::distance(x = bedfile, y = cds[[tx]])
-    dist <- min(out)
-    side <- GenomicRanges::follow(subject = bedfile, x = cds[[tx]])[which.min(out)]
-    exon <- cds[[tx]]$exon_rank[which.min(out)]
-
-    # if positive strand
-    if (GenomicRanges::strand(cds[[tx]])[1] %>% as.vector() == '+') {
-      # event occuring upstream of cds
-      if (is.na(side)) {
-        # interval is downstream of exon
-        # hgvs includes nearest exon
-        entry_cds <- GenomicRanges::width(cds[[tx]][1:which.min(out)]) %>% sum
-        hgvs <- paste0("c.", entry_cds, "+", dist)
-      } else {
-        # hgvs should not include nearest exon. +1 because in relation to first base of
-        # nearest exon
-        entry_cds <- 1 + GenomicRanges::width(cds[[tx]][1:which.min(out)-1]) %>% sum
-        hgvs <- paste0("c.", entry_cds, "-", dist+1)
-      }
-    } else {
-      # if negative strand
-      if (is.na(side)) {
-        # interval is downstream of exon
-        # hgvs should include this exon
-        entry_cds <- GenomicRanges::width(cds[[tx]][1:which.min(out)]) %>% sum
-        hgvs <- paste0("c.", entry_cds, "+", dist + 1)
-      } else {
-        # interval is upstream of exon
-        # hgvs shouls not includes exon. +1 because in relation to first base of nearest exon
-        entry_cds <- 1 + GenomicRanges::width(cds[[tx]][1:which.min(out) - 1]) %>% sum
-        hgvs <- paste0("c.", entry_cds, "-", dist + 1) #
-      }
-
-    }
-
-    data.frame(tx, exon, hgvs) %>% return()
-    })
-
-  do.call(rbind, out) %>%
-    return()
-  }
-
 
 getHgvs <- function(bedfile, cds_by_tx) {
 
   # index of bed overlap with cds
   bed_ol_tx <- IRanges::findOverlaps(query = bedfile, subject = cds_by_tx)
 
-  # extract transcripts and index by bed entry
+  # if no overlaps across all bed regions - stop
+
+  # get transcript index that overlap each bedentry
+  # output should contain hgvs for these
   cds_ol <- lapply(seq(bedfile), function(x) {
     S4Vectors::queryHits(bed_ol_tx) %in% x %>%
       bed_ol_tx[.] %>%
-      S4Vectors::subjectHits(.) %>%
-      cds_by_tx[.]
+      S4Vectors::subjectHits(.)
   })
 
   # loop over each element (bed entry)
   lapply(seq(cds_ol), function(bedln) {
 
+    chr   <- GenomicRanges::seqnames(bedfile[bedln]) %>% as.vector()
+    start <- GenomicRanges::start(bedfile[bedln])
+    end   <- GenomicRanges::end(bedfile[bedln])
+
     # if not empty
     if(!isEmpty(cds_ol)[bedln]) {
 
-      # all transcripts for bed entry
-      tx   <- names(cds_ol[[bedln]])
-
-      # loop over tx entry - get exon & hgvs
-      cds_annot <- lapply(names(cds_ol[[bedln]]), function(tx) {
-        mapCoordToCds(range = bedfile[bedln], cds = cds_ol[[bedln]][[tx]])
+      # loop over tx index - get exon & hgvs
+      cds_annot <- lapply(cds_ol[[bedln]], function(tx_i) {
+        mapCoordToCds(bedfile = bedfile[bedln], cds = cds_by_tx[[tx_i]])
       })
 
-      exon  <- lapply(cds_annot, function(x) {x$exon_rank}) %>% unlist()
-      hgvs  <- lapply(cds_annot, function(x) {x$cds}) %>% unlist() %>% paste0("c.", .)
-
-    } else {
-      tx   <- NA
-      exon <- NA
-      hgvs <- NA
+      tx         <- names(cds_by_tx)[cds_ol[[bedln]]]
+      hgvs_start <- lapply(cds_annot, function(x) {x$start$hgvs}) %>% unlist()
+      hgvs_end   <- lapply(cds_annot, function(x) {x$end$hgvs}) %>% unlist()
+      exon_start <- lapply(cds_annot, function(x) {x$start$exon_rank}) %>% unlist()
+      exon_end   <- lapply(cds_annot, function(x) {x$end$exon_rank}) %>% unlist()
     }
 
-    data.frame(tx, exon, hgvs)
+    data.frame(chr, start, end, tx, hgvs_start, exon_start, hgvs_end, exon_end)
   })
 
 }
@@ -188,32 +87,90 @@ getHgvs <- function(bedfile, cds_by_tx) {
 #' @param cds
 #'
 #' @return
-mapCoordToCds <- function(range, cds) {
+mapCoordToCds <- function(bedfile, cds) {
+
+  # make separate ranges object for start and end coordinates
+  bedfile_start <- IRanges::narrow(x = bedfile, start = 1, width = 1)
+  bedfile_end   <- IRanges::narrow(x = bedfile, start = -1, width = 1)
+
+  hgvs_start <- getHgvs2(bedfile = bedfile_start, cds = cds)
+  hgvs_end   <- getHgvs2(bedfile = bedfile_end, cds = cds)
+
+  list("start" = hgvs_start, "end" = hgvs_end) %>% return()
+}
+
+
+
+
+
+getHgvs2 <- function(bedfile, cds) {
 
   # find which exon overlaps
-  hits <- IRanges::findOverlaps(query = range, subject = cds)
+  dist_to_cds <- IRanges::distance(x = bedfile, y = cds)
+
+  # which exon
+  near_ex_i <- which.min(dist_to_cds)
+
+  # get dist to event (0-based - if using in HGVS need to add 1bp)
+  dist <- dist_to_cds[near_ex_i]
 
   # get that exon
-  cds_hit_ln <-cds[S4Vectors::subjectHits(hits)]
+  near_ex <- cds[near_ex_i]
 
-  # how far into exon hit is bed entry
-  # for positive strand
-  if (GenomicRanges::strand(cds_hit_ln) %>% as.vector() == '+') {
-    cds_width_hit <- IRanges::start(range) - IRanges::start(cds_hit_ln)
-    # accum width of cds upstream of exon for + strand
-    cds_width_us <- IRanges::width(cds)[1:(S4Vectors::subjectHits(hits)) - 1] %>% sum()
+  # exon number
+  exon_rank <- near_ex$exon_rank
+
+  # which strand
+  strand  <- GenomicRanges::strand(near_ex) %>% as.vector()
+
+  if ( dist == 0 ) {
+    # coordinate within cds
+    if ( strand == "+") {
+      # on positive strand
+      # distance into cds hit
+      cds_width_hit <- (1 + IRanges::start(bedfile) - IRanges::start(near_ex))
+      # accum width of cds upstream of exon for + strand
+      cds_width_us <- IRanges::width(cds)[1:near_ex_i - 1] %>% sum()
+      hgvs <- paste0("c.", sum(cds_width_hit + cds_width_us))
+    } else {
+      # on negative strand
+      # distance into cds hit
+      cds_width_hit <- (1 + IRanges::end(near_ex) - IRanges::start(bedfile))
+      # accum width of cds upstream of exon for - strand
+      cds_width_us <- IRanges::width(cds)[1:near_ex_i - 1] %>% sum()
+      hgvs <- paste0("c.", sum(cds_width_hit + cds_width_us))
+    }
   } else {
-    # negative strand
-    cds_width_hit <- IRanges::end(cds_hit_ln) - IRanges::start(range)
-    cds_width_us <- IRanges::width(cds)[1:(S4Vectors::subjectHits(hits)) - 1] %>% sum()
+    # coordinate is flanking cds
+    # if positive strand
+    if (strand == '+') {
+      # event occuring upstream of cds
+      if (IRanges::start(bedfile) < IRanges::start(near_ex)) {
+        # interval is upstream of exon. hgvs should not include nearest exon
+        # +1 because in relation to first base of upstream exon
+        entry_cds <- 1 + IRanges::width(cds[1:near_ex_i - 1]) %>% sum
+        hgvs <- paste0("c.", entry_cds, "-", dist + 1)
+      } else {
+        # downstream, hgvs includes nearest exon
+        entry_cds <- IRanges::width(cds[1:(near_ex_i)]) %>% sum
+        hgvs <- paste0("c.", entry_cds, "+", dist + 1)
+      }
+    } else {
+      # if negative strand
+      if (IRanges::start(bedfile) < IRanges::start(near_ex)) {
+        # interval is downstream of exon
+        # hgvs should include this exon
+        entry_cds <- IRanges::width(cds[1:near_ex_i]) %>% sum
+        hgvs <- paste0("c.", entry_cds, "+", dist + 1)
+      } else {
+        # interval is upstream of exon
+        # hgvs shouls not includes exon. +1 because in relation to first base of nearest exon
+        entry_cds <- 1 + IRanges::width(cds[1:near_ex_i - 1]) %>% sum
+        hgvs <- paste0("c.", entry_cds, "-", dist + 1)
+      }
+    }
   }
-
-  # total cds
-  cds_width_final <- sum(cds_width_us, cds_width_hit) + 1
-
-  cds_hit_ln$cds <- cds_width_final
-
-  return(cds_hit_ln)
+  return(list("hgvs" = hgvs, "exon_rank" = exon_rank))
 }
 
 
@@ -228,6 +185,7 @@ getSymbolRefseq <- function(refSeqId) {
   # remove suffix
   stringr::str_remove(string = refSeqId, pattern = "\\.\\d+") %>%
     AnnotationDbi::select(org.Hs.eg.db, keytype = "REFSEQ", keys = ., columns = "SYMBOL") %>%
+    .[,"SYMBOL"] %>%
     return()
 
 }
--- tests/testthat/test-Rbed2HGVS.R
+++ tests/testthat/test-Rbed2HGVS.R
@@ -5,10 +5,10 @@ test_that("cds gap that finishes downstream on positive strand", {
     db = '/home/chris/Apps/Rbed2HGVS/data/ucsc_hg19_ncbiRefSeq.sqlite'
     )
 
-  testthat::expect_equal(out$hgvs.start %>% as.vector(), "c.6829")
-  testthat::expect_equal(out$exon.start, 11)
-  testthat::expect_equal(out$hgvs.end %>% as.vector(), "c.6841+18")
-  testthat::expect_equal(out$exon.end, 11)
+  testthat::expect_equal(out$hgvs_start %>% as.vector(), "c.6829")
+  testthat::expect_equal(out$exon_start, 11)
+  testthat::expect_equal(out$hgvs_end %>% as.vector(), "c.6841+19")
+  testthat::expect_equal(out$exon_end, 11)
 })
 
 test_that("cds gap that finishes downstream on negative strand", {
@@ -19,18 +19,18 @@ test_that("cds gap that finishes downstream on negative strand", {
   )
 
   testthat::expect_equal(
-    out$hgvs.start %>% as.vector(),
+    out$hgvs_start %>% as.vector(),
     c("c.1100+14","c.1100+14","c.704+14","c.983+14","c.623+14","c.983+14","c.983+14")
     )
 
-  testthat::expect_equal(out$exon.start, c(10,10,6,9,6,10,10))
+  testthat::expect_equal(out$exon_start, c(10,10,6,9,6,10,10))
 
   testthat::expect_equal(
-    out$hgvs.end %>% as.vector(),
+    out$hgvs_end %>% as.vector(),
     c("c.1087","c.1087","c.691","c.970","c.610","c.970","c.970")
     )
 
-  testthat::expect_equal(out$exon.end, c(10,10,6,9,6,10,10))
+  testthat::expect_equal(out$exon_end, c(10,10,6,9,6,10,10))
 })
 
 
@@ -41,10 +41,10 @@ test_that("cds gap that finishes upstream on positive strand", {
     db = '/home/chris/Apps/Rbed2HGVS/data/ucsc_hg19_ncbiRefSeq.sqlite'
   )
 
-  testthat::expect_equal(out$hgvs.start %>% as.vector(), "c.1910-30")
-  testthat::expect_equal(out$exon.start, 11)
-  testthat::expect_equal(out$hgvs.end %>% as.vector(), "c.1939")
-  testthat::expect_equal(out$exon.end, 11)
+  testthat::expect_equal(out$hgvs_start %>% as.vector(), "c.1910-30")
+  testthat::expect_equal(out$exon_start, 11)
+  testthat::expect_equal(out$hgvs_end %>% as.vector(), "c.1939")
+  testthat::expect_equal(out$exon_end, 11)
 })
 
 
@@ -56,17 +56,17 @@ test_that("cds gap that finishes upstream on negative strand", {
   )
 
   testthat::expect_equal(
-    out$hgvs.start %>% as.vector(),
+    out$hgvs_start %>% as.vector(),
     c("c.1009","c.1009","c.613","c.892","c.532","c.892","c.892")
     )
 
-  testthat::expect_equal(out$exon.start, c(10, 10, 6, 9, 6, 10, 10))
+  testthat::expect_equal(out$exon_start, c(10, 10, 6, 9, 6, 10, 10))
 
   testthat::expect_equal(
-    out$hgvs.end %>% as.vector(),
+    out$hgvs_end %>% as.vector(),
     c("c.994-19", "c.994-19", "c.598-19", "c.877-19", "c.517-19", "c.877-19", "c.877-19")
     )
 
-  testthat::expect_equal(out$exon.end, c(10, 10, 6, 9, 6, 10, 10))
+  testthat::expect_equal(out$exon_end, c(10, 10, 6, 9, 6, 10, 10))
 })
 
